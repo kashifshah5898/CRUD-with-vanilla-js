eb2f7e4dd1932bd4a62f34f9081487ddc894e804
kashif <kashifshah5898@gmail.com>
2023-04-15 03:32:44 +0500

    changing logic

--- js/uploadDocument.js
+++ js/uploadDocument.js
@@ -29,27 +29,116 @@ const settingAffiliation = () => {
   });
 };
 
-const remove = () => {
-  console.log("Remove called");
+const handleDeleteButtonClick = (id) => {
+  // let ids = id - 2;
+  // allAuthors.splice(ids - 1, 1);
+  // const removeElement = document.getElementsByClassName(ids);
+  // Array.from(removeElement).forEach(function (element) {
+  //   element.remove();
+  // });
+  // console.log(allAuthors);
+  const element = document.getElementById(id);
+  console.log(id);
+  if (element) {
+    element.parentNode.removeChild(element);
+  }
 };
+
+const showAllAuthors = (aName) => {
+  const showAllAuthorsDiv = document.querySelector("#showAllAuthorsId");
+  const authorNameParagraph = document.createElement("p");
+  authorNameParagraph.textContent = allAuthors.length + ". " + aName;
+
+  authorNameParagraph.setAttribute("id", "author-" + allAuthors.length);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id", "delete-" + allAuthors.length);
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add("btn");
+  deleteButton.classList.add("btn-danger");
+  deleteButton.classList.add("ml30");
+  deleteButton.classList.add("ml-2");
+  deleteButton.addEventListener("click", function () {
+    // handleDeleteButtonClick(allAuthors.length);
+    handleDeleteButtonClick("author-" + allAuthors.length);
+    handleDeleteButtonClick("delete-" + allAuthors.length);
+  });
+
+  const breakLine = document.createElement("br");
+
+  showAllAuthorsDiv.appendChild(authorNameParagraph);
+
+  showAllAuthorsDiv.appendChild(deleteButton);
+  showAllAuthorsDiv.appendChild(breakLine);
+};
+
+let allAuthors = [];
 const addAuthor = () => {
-  console.log("addAuthor called");
+  if (firstName.value && lastName.value && email.value && affiliation.value) {
+    let author = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      affiliation: affiliation.value,
+      presenter: presenter.checked,
+    };
+
+    allAuthors.push(author);
+    firstName.value = "";
+    lastName.value = "";
+    email.value = "";
+    affiliation.value = "";
+    presenter.checked = false;
+
+    showAllAuthors(author.firstName);
+  } else {
+    alert("Please fill all fields");
+  }
 };
 
 const form = document.getElementById("form");
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   // Prevent the default form submission behavior
   event.preventDefault();
-  console.log(`
-  title: ${title.value}
-abstract: ${abstract.value}
-firstName: ${firstName.value}
-lastName: ${lastName.value}
-email: ${email.value}
-affiliation: ${affiliation.value}
-presenter: ${presenter.checked}
-file: ${file.value}`);
 
-  // Reset the form to its initial state
-  // form.reset();
+  let anyTrue = 0;
+  allAuthors.map((item) => {
+    if (item?.presenter) {
+      anyTrue += 1;
+    }
+  });
+
+  if (!anyTrue) {
+    return alert("Add atlease one author as presenter");
+  }
+
+  const usersRes = await fetch("users.json");
+  const users = await usersRes.json();
+
+  const numIndexes = 2;
+  const randomIndexes = [];
+
+  while (randomIndexes.length < numIndexes) {
+    const randomIndex = Math.floor(Math.random() * users.length);
+    if (!randomIndexes.includes(randomIndex)) {
+      randomIndexes.push(randomIndex);
+    }
+  }
+
+  const myObj = {
+    title: title.value,
+    abstract: abstract.value,
+    file: file.value,
+    authors: allAuthors,
+    reviewers: [
+      users[randomIndexes[0]].first_name + " " + users[randomIndexes[0]].last_name,
+      users[randomIndexes[1]].first_name + " " + users[randomIndexes[1]].last_name,
+    ],
+  };
+
+  const data = JSON.stringify(myObj);
+
+  console.warn("data to write in json: ", data);
+  allAuthors = [];
+  form.reset();
 });
